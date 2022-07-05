50004b9b261aa0223c95dac0d5b87d72423e11f8
Michael <michaelkmb@hotmail.com>
2022-07-05 17:19:24 -0300

    feat localStorage for items;

--- js/main.js
+++ js/main.js
@@ -1,42 +1,89 @@
 (() => {
   const form = document.querySelector(".adicionar");
 
+  const nameInp = document.querySelector("[data-inp='name']");
+  const qttInp = document.querySelector("[data-inp='quantity']");
+
   const list = document.querySelector("[data-list]");
   let listObj = {};
   let listItem = [];
 
-  let resetItem = () => {
-    listObj.name = "";
-    listObj.qtt = 0;
+  //clear list
+  let resetList = () => {
+    list.innerHTML = "";
   };
-  resetItem();
 
-  let addItem = (event) => {
-    event.preventDefault();
+  //clear inputs
+  let resetItem = () => {
+    nameInp.value = "";
+    qttInp.valueAsNumber = 1;
+  };
 
-    listObj.name = event.target.elements["nome"].value;
-    listObj.qtt = event.target.elements["quantidade"].valueAsNumber;
+  //on init functions to get local storage
+  let getItemLS = (item) => {
+    resetItem();
+    listObj.name = item.name;
+    listObj.qtt = item.qtt;
+  };
 
-    listItem.push(listObj);
+  let getList = () => {
+    const storage = localStorage.getItem("listItems") || [];
+
+    if (storage.length == 0) return;
+    resetList();
+
+    const parsed = JSON.parse(storage);
+    for (item of parsed) {
+      getItemLS(item);
+      listItem.push({ name: listObj.name, qtt: listObj.qtt });
+    }
+
     updateList();
   };
 
+  //to update actual object with input values
+  let updateItem = (event) => {
+    listObj.name = event.target.elements["nome"].value;
+    listObj.qtt = event.target.elements["quantidade"].valueAsNumber;
+    resetItem();
+  };
+
+  //add input value to list and local storage list
+  let addItem = (event) => {
+    event.preventDefault();
+
+    updateItem(event);
+    console.log(`push on addItem ${JSON.stringify(listItem)}`);
+    listItem.push({ name: listObj.name, qtt: listObj.qtt });
+
+    console.log(`addItem listItem ${JSON.stringify(listItem)}`);
+    localStorage.setItem("listItems", JSON.stringify(listItem));
+
+    updateList();
+
+    console.log("####################");
+  };
+
+  //create html element node
   let createItemList = (item) => {
     const node = document.createElement("li");
-
     node.classList.add("item");
-    node.textContent = item.name;
 
     const qtt = document.createElement("strong");
     qtt.textContent = item.qtt;
 
+    const name = document.createElement("span");
+    name.innerText = item.name;
+
     node.appendChild(qtt);
+    node.appendChild(name);
 
     return node;
   };
 
+  //put list items on html
   let updateList = () => {
-    list.innerHTML = "";
+    resetList();
     listItem.forEach((item) => {
       const node = createItemList(item);
 
@@ -44,5 +91,8 @@
     });
   };
 
+  //on js startup, do somethings
+  resetItem();
+  getList();
   form.addEventListener("submit", addItem);
 })();
